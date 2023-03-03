e70372b1d9d28bbed60f7ad7570c4e18255b7add
Thomas Obadia <thomas.obadia@gmail.com>
2023-03-03 18:13:25 +0100

    Update examples with some slight todying, but not too much.

--- tests/generation.time.R
+++ tests/generation.time.R
@@ -3,7 +3,7 @@ library(R0)
 
 # GT for children at house(from Cauchemez PNAS 2011)
 
-GT.chld.hsld1<-generation.time("empirical", c(0,0.25,0.2,0.15,0.1,0.09,0.05,0.01))
+GT.chld.hsld1 <- generation.time("empirical", c(0,0.25,0.2,0.15,0.1,0.09,0.05,0.01))
 plot(GT.chld.hsld1, col="green")
 GT.chld.hsld1
 # Discretized Generation Time distribution
@@ -11,7 +11,7 @@ GT.chld.hsld1
 # [1] 0.00000000 0.29411765 0.23529412 0.17647059 0.11764706 0.10588235 0.05882353
 # [8] 0.01176471
 
-GT.chld.hsld2<-generation.time("gamma", c(2.45, 1.38))
+GT.chld.hsld2 <- generation.time("gamma", c(2.45, 1.38))
 GT.chld.hsld2
 # Discretized Generation Time distribution
 # mean: 2.504038 , sd: 1.372760
@@ -21,7 +21,7 @@ GT.chld.hsld2
 
 
 # GT for school & community
-GTs1<-generation.time("empirical", c(0,0.95,0.05))
+GTs1 <- generation.time("empirical", c(0,0.95,0.05))
 plot(GTs1, col='blue')
 
 
