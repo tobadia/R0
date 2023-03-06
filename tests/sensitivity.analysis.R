15dafeb4ab82ad11e1a0c59ddf1ffaa8890439bf
Thomas Obadia <thomas.obadia@gmail.com>
2023-03-06 13:26:41 +0100

    Fix an issue in the examples for sensitivity.analysis() where some lines were over 100 characters, throwing a NOTE in R CMD CHECK --as-cran

--- tests/sensitivity.analysis.R
+++ tests/sensitivity.analysis.R
@@ -12,14 +12,37 @@ data(Germany.1918)
 ## Here we will test GT with means of 1 to 5, each time with SD constant (1)
 ## GT and SD can be either fixed value or vectors of values
 ## Actual value in simulations may differ, as they are adapted according to the distribution type
-tmp <- sensitivity.analysis(sa.type="GT", incid=Germany.1918, GT.type="gamma", GT.mean=seq(1,5,1), 
-                          GT.sd.range=1, begin=1, end=27, est.method="EG")
+tmp <- sensitivity.analysis(
+     sa.type     = "GT", 
+     incid       = Germany.1918, 
+     GT.type     = "gamma", 
+     GT.mean     = seq(1,5,1), 
+     GT.sd.range = 1, 
+     begin       = 1, 
+     end         = 27, 
+     est.method  = "EG")
 
 ## Results are stored in a matrix, each line dedicated to a (mean,sd) couple
-plot(x = tmp[,"GT.Mean"], xlab = "mean GT (days)", y = tmp[,"R"], ylim = c(1.2, 2.1), ylab = "R0 (95% CI)", 
-     type = "p", pch = 19, col = "black", main = "Sensitivity of R0 to mean GT")
-arrows(x0 = as.numeric(tmp[,"GT.Mean"]), y0 = as.numeric(tmp[,"CI.lower"]), 
-       y1 = as.numeric(tmp[,"CI.upper"]), angle = 90, code = 3, col = "black", length = 0.05)
+plot(
+     x    = tmp[,"GT.Mean"], 
+     xlab = "mean GT (days)", 
+     y    = tmp[,"R"], 
+     ylim = c(1.2, 2.1), 
+     ylab = "R0 (95% CI)", 
+     type = "p", 
+     pch  = 19, 
+     col  = "black", 
+     main = "Sensitivity of R0 to mean GT"
+)
+arrows(
+     x0 = as.numeric(tmp[,"GT.Mean"]), 
+     y0 = as.numeric(tmp[,"CI.lower"]), 
+     y1 = as.numeric(tmp[,"CI.upper"]), 
+     angle = 90, 
+     code = 3, 
+     col = "black", 
+     length = 0.05
+)
 ## One could tweak this example to change sorting of values (per mean, or per standard deviation)
 ## eg: 'x=tmp[,c('GT.Mean')]' could become 'x=tmp[,c('GT.SD')]'
 
@@ -27,8 +50,14 @@ arrows(x0 = as.numeric(tmp[,"GT.Mean"]), y0 = as.numeric(tmp[,"CI.lower"]),
 ## sa.type="time"
 
 mGT <- generation.time("gamma", c(2.6,1))
-sen <- sensitivity.analysis(sa.type = "time", incid = Germany.1918, GT = mGT, begin = 1:10, end = 16:25, 
-                         est.method = "EG")
+sen <- sensitivity.analysis(
+     sa.type    = "time", 
+     incid      = Germany.1918, 
+     GT         = mGT, 
+     begin      = 1:10, 
+     end        = 16:25, 
+     est.method = "EG"
+)
 # ...
 # Warning message:
 # If 'begin' and 'end' overlap, cases where begin >= end are skipped.
